0c099044c00ab410eb19966b1fc80e1c1e705994
Lawson Han <lawsonhan123@gmail.com>
2026-02-04 18:33:59 +0800

    feat: Update Vite configuration to set dynamic base path for build and development environments, and adjust BrowserRouter to use the new BASE_URL.

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,12 @@ import react from '@vitejs/plugin-react'
 import path from "path"
 import tailwindcss from "@tailwindcss/vite"
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
+  base: command === "build" ? "/CMS-demo/" : "/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+}))
